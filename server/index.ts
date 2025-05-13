e532660e44d9fcdecd781a759d03ccd28bfa8e56
Rutendo0 <rutendomikitayo45@gmail.com>
2025-05-13 12:01:17 +0200

    Fix Vercel deployment structure

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
-import path from 'path';
+import path, { join } from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
